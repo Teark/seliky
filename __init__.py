6a31d61870a2d84489bd995823b77eaef2f6afc4
teark <junxiong.liu@vcg.com>
2021-09-01 18:37:04 +0800

    .

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from WebDriver2 import *
+
+name = "seliky"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seliky",
+    version="0.3",
+    author="TEARK",
+    author_email="913355434@qq.com",
+    description="a better lib based on selenium",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/teark/seliky.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
