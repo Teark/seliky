610e0571b99bc0c6535e9a8b84f6f9653067b42d
teark <junxiong.liu@vcg.com>
2021-09-08 13:37:57 +0800

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="seliky",
-    version="0.9",
+    version="0.12",
     author="TEARK",
     author_email="913355434@qq.com",
     description="a better ui autotest lib based on selenium, compatible with robot framework",
@@ -13,9 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://gitee.com/teark/seliky.git",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+        'pyvirtualdisplay',
+        'colorama'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    python_requires='>=3.7',
 )
